23a67630f8bc8f7b9704d23ff73e31e899a083e7
Khoi Nguyen <nguyenkhoi2004pq@gmail.com>
2024-10-16 22:14:05 +0700

    khoi da lam xong task so 5

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,44 +1,46 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faPhone } from "@fortawesome/free-solid-svg-icons";
+  "use client"
+  import Link from 'next/link';
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () => {
-  return (
-    <div>
-      <header className="bg-gray-800">
-        <div className="border-b border-gray-600">
-          <div className="mx-[175px] flex flex-col md:flex-row items-start md:items-center justify-between py-2 px-4">
-            <div className="flex items-start justify-center">
-              <p className="text-gray-300 text-sm leading-9 md:leading-10">World Wide Completely Free Returns and Shipping</p>
+  const Header = () => {
+    return (
+      <div>
+        <header className="bg-gray-800">
+          <div className="border-b border-gray-600">
+            <div className="mx-[175px] flex flex-col md:flex-row items-start md:items-center justify-between py-2 px-4">
+              <div className="flex items-start justify-center">
+                <p className="text-gray-300 text-sm leading-9 md:leading-10">World Wide Completely Free Returns and Shipping</p>
+              </div>
+              <nav className="flex items-end flex-col">
+                <ul className="flex items-end">
+                  <li className="mr-4 pr-4 border-r border-gray-600">
+                    <a href="#" className="text-gray-300 text-sm flex items-center hover:text-blue-500">
+                      <FontAwesomeIcon icon={faPhone} className="mr-1 w-4 h-4" />
+                      +012 3456 789
+                    </a>
+                  </li>
+                  <li className="mr-4 pr-4 border-r border-gray-600">
+                    <a href="#" className="text-gray-300 text-sm flex items-center hover:text-blue-500">
+                      <FontAwesomeIcon icon={faPhone} className="mr-1 w-4 h-4" />
+                      demo@example.com
+                    </a>
+                  </li>
+                  <li className="mr-4 pr-4 border-gray-600">
+                    <Link href="/account" legacyBehavior>
+                      <a className="text-gray-300 text-sm flex items-center hover:text-blue-500">
+                        <FontAwesomeIcon icon={faPhone} className="mr-1 w-4 h-4" />
+                        Tài khoản
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <nav className="flex items-end flex-col">
-              <ul className="flex items-end">
-                <li className="mr-4 pr-4 border-r border-gray-600">
-                  <a href="#" className="text-gray-300 text-sm flex items-center hover:text-blue-500">
-                    <FontAwesomeIcon icon={faPhone} className="mr-1 w-4 h-4" />
-                    +012 3456 789
-                  </a>
-                </li>
-                <li className="mr-4 pr-4 border-r border-gray-600">
-                  <a href="#" className="text-gray-300 text-sm flex items-center hover:text-blue-500">
-                    <FontAwesomeIcon icon={faPhone} className="mr-1 w-4 h-4" />
-                    demo@example.com
-                  </a>
-                </li>
-                <li className="mr-4 pr-4 border-gray-600">
-                  <a href="#" className="text-gray-300 text-sm flex items-center hover:text-blue-500">
-                    <FontAwesomeIcon icon={faPhone} className="mr-1 w-4 h-4" />
-                 Account
-                  </a>
-                </li>
-              </ul>
-            </nav>
           </div>
-        </div>
+        </header>
+      </div>
+    );
+  };
 
-      
-      </header>
-    </div>
-  );
-};
-
-export default Header;
+  export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import Link from "next/link";
 // import Link from "next/link";
 // import classNames from "classnames/bind";
 
