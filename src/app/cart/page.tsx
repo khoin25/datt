0f7f88da3980c09d796f658547d6fd0abbbbaa3b
Pham Duy <duyp7454@gmail.com>
2024-10-17 17:47:44 +0700

    done cart responsive

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,133 +1,169 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./css/cart.css"
+import "./css/cart.css";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 
 export default function Cart() {
-    return <div className="w-[80%] mx-auto py-4">
+  return (
+    <div className="2xl:w-[80%] mx-auto py-4 sm:w-[95%] md:-[90%]">
       <h1 className="text-[1.4rem] font-semibold py-3">Your cart items</h1>
-    <table className="w-[100%] table-cart">
-  <thead className="w-[100%] bg-[#266bf9] text-white h-[3rem] text-center">
-    <tr>
-      <th>Image</th>
-      <th>Product Name</th>
-      <th>Until Price</th>
-      <th>Qty</th>
-      <th>Subtotal</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody className="">
-    <tr className="">
-      <td className="product-thumbnail w-[150px]">
-        <a href="#"><img className="p-[1rem] max-w-[100%] ml-15px" src="https://template-intern.l5elb4sxvvqkvl.flashvps.xyz/Tech/hmart-electronics-ecommerce-html-template-2023-11-27-05-11-08-utc/hmart/assets/images/product-image/1.webp" alt /></a>
-      </td>
-      <td className="product-name"><a href="#">Modern Smart Phone</a></td>
-      <td className="product-price-cart"><span className="amount">$60.00</span></td>
-      <td className="product-quantity flex justify-center items-center h-[150px]">
-      <div className='text-[1rem] border border-black flex items-center w-[5.5rem] justify-between'>
-        <div className='border  text-[1rem] px-1 border-r-[black] py-1 cursor-pointer'>
-          -
+      <div className=" xl:flex justify-between">
+        {/* Cart Items Table */}
+        <div className="2xl:w-[70%] xl:w-[63%]">
+          {/* laptop */}
+          <table className="w-[100%] table-cart sm:hidden md:block">
+            <thead className="bg-[#266bf9] text-white h-[3rem] text-center">
+              <tr>
+                <th>Image</th>
+                <th>Product Name</th>
+                <th >Unit Price</th>
+                <th >Qty</th>
+                <th>Subtotal</th>
+                <th >Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="product-thumbnail sm:w-[75px] sxm:w-[100px] md:w-[150px]">
+                  <a href="#">
+                    <img
+                      className="p-[1rem] max-w-[100%] ml-[15px]"
+                      src="https://template-intern.l5elb4sxvvqkvl.flashvps.xyz/Tech/hmart-electronics-ecommerce-html-template-2023-11-27-05-11-08-utc/hmart/assets/images/product-image/1.webp"
+                      alt="Product"
+                    />
+                  </a>
+                </td>
+                <td className="product-name sm:text-[.8rem] sxm:-[1rem]">
+                  <a href="#">Modern Smart Phone</a>
+                </td>
+                <td className="product-price-cart  ">
+                  <span className="amount">$60.00</span>
+                </td>
+                <td className="product-quantity md:flex justify-center items-center h-[150px]">
+                  <div className="text-[1rem] border border-black flex items-center w-[5.5rem] justify-between">
+                    <div className="border px-1 border-r-[black] py-1 cursor-pointer">-</div>
+                    <input
+                      type="text"
+                      value={1}
+                      className="text-center focus:outline-none bg-transparent w-[100%] px-[.8rem] text-[.9rem]"
+                      min={1}
+                      max={100}
+                    />
+                    <div className="border px-1 border-l-[black] py-1 cursor-pointer">+</div>
+                  </div>
+                </td>
+                <td className="product-subtotal text-red-500">$70.00</td>
+                <td className="product-remove cursor-pointer sm:hidden md:block">
+                  <FontAwesomeIcon icon={faClose} className="w-4 h-4 m-auto" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+       {/* Mobile */}
+       <table className="w-[100%] table-cart md:hidden">
+            <thead className="bg-[#266bf9] text-white h-[3rem] text-center">
+              <tr>
+                <th>Image</th>
+                <th>Product Name</th>
+        
+                <th>Subtotal</th>
+             
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="product-thumbnail sm:w-[75px] sxm:w-[100px] md:w-[150px]">
+                  <a href="#">
+                    <img
+                      className="p-[1rem] max-w-[100%] ml-[15px]"
+                      src="https://template-intern.l5elb4sxvvqkvl.flashvps.xyz/Tech/hmart-electronics-ecommerce-html-template-2023-11-27-05-11-08-utc/hmart/assets/images/product-image/1.webp"
+                      alt="Product"
+                    />
+                  </a>
+                </td>
+                <td className="product-name sm:text-[.8rem] sxm:-[1rem]">
+                  <a href="#">Modern Smart Phone</a>
+                </td>
+             
+           
+                <td className="product-subtotal text-red-500">$70.00</td>
+               
+              </tr>
+            </tbody>
+          </table>
+          {/* Cart Buttons */}
+          <div className="flex justify-between py-[1rem]">
+            <button className="py-[.4rem] sm:px-[1rem] sm:text-[.8rem]  sxm:px-[1.3rem] bg-white border border-[#266bf9] text-[#266bf9] text-[1rem] flex justify-center items-center transition-all hover:bg-[#266bf9] hover:text-white">
+              Continue Shopping
+            </button>
+            <div className="flex gap-[1rem]">
+              <button className="py-[.4rem] sm:hidden md:block px-[1.3rem] border border-[#266bf9] text-[#266bf9] sxm:text-[1rem] flex justify-center items-center transition-all hover:bg-[#266bf9] hover:text-white">
+                Update Shopping Cart
+              </button>
+              <button className="py-[.4rem] sm:px-[1rem] sm:text-[.8rem]  sxm:px-[1.3rem] bg-[#266bf9] text-white sxm:text-[1rem] flex justify-center items-center transition-all hover:bg-[#0055ff]">
+                Clear Shopping Cart
+              </button>
+            </div>
           </div>
-        <input type="text" value={1} className="text-center focus:outline-none bg-transparent w-[100%] px-[.8rem] text-[.9rem]" min={1} max={100}/>
-        <div className='border text-[1rem] px-1 border-l-[black] py-1 cursor-pointer'>+</div>
-      </div>
-  
-      </td>
-      <td className="product-subtotal text-red-500">$70.00</td>
-      <td className="product-remove cursor-pointer ">
-     <FontAwesomeIcon icon={faClose} className="w-4 h-4 m-auto"/>
-      </td>
-    </tr>
- 
-  </tbody>
-</table>
-<div className="flex justify-between py-[1rem]">
-  <button className="py-[.4rem] px-[1.3rem] bg-white border border-[#266bf9] text-[#266bf9] text-[1rem] flex justify-center items-center transition-all hover:bg-[#266bf9] hover:text-white">Continue Shopping</button>
-  <div className="flex gap-[1rem]">
-    <button className="py-[.4rem] px-[1.3rem] border border-[#266bf9] text-[#266bf9] text-[1rem] flex justify-center items-center transition-all hover:bg-[#266bf9] hover:text-white">Update Shopping Cart</button>
-    <button className="py-[.4rem] px-[1.3rem] bg-[#266bf9] text-white text-[1rem] flex justify-center items-center transition-all hover:bg-[#0055ff] hover:text-white">Clear Shopping Cart</button>
-  </div>
-</div>
-<div className="flex justify-between">
-<div className="cart-tax w-[30%]  border border-[#266bf9] shadow-sm bg-white px-[2rem] py-[2rem] text-black">
-  <div className="title-wrap">
-    <h4 className="font-semibold mb-[1rem] text-[1rem]">Estimate Shipping And Tax</h4>
-  </div>
-  <div className="tax-wrapper">
-    <p className="mb-[.8rem] text-[.9rem] text-gray-400">Enter your destination to get a shipping estimate.</p>
-    <div className="tax-select-wrapper">
-      <div className="tax-select pb-[.5rem]">
-        <label className="block mb-[.5rem]">
-          * Country
-        </label>
-        <select className="w-[100%] border border-black py-[.4rem] px-3">
-          <option>Bangladesh</option>
-          <option>Albania</option>
-          <option>Åland Islands</option>
-          <option>Afghanistan</option>
-          <option>Belgium</option>
-        </select>
-      </div>
-      <div className="tax-select">
-        <label className="block mt-[.5rem] mb-[.5rem]">
-          * Region / State
-        </label>
-        <select className="w-[100%] border border-black py-[.4rem] px-3">
-          <option>Bangladesh</option>
-          <option>Albania</option>
-          <option>Åland Islands</option>
-          <option>Afghanistan</option>
-          <option>Belgium</option>
-        </select>
-      </div>
-      <div className="tax-select mb-25px">
-      <label className="block mt-[.5rem] mb-[.5rem]">
-      * Zip/Postal Code
-        </label>
-        <input type="text" className="w-[100%] border border-black py-[.3rem] px-3"/>
-      </div>
-      <button className="cart-btn-2 mt-[1rem]" type="submit">Get A Quote</button>
-    </div>
-  </div>
-</div>
-<div className="cart-tax w-[30%]  border border-[#266bf9] shadow-sm h-[100%] bg-white px-[2rem] py-[2rem] text-black">
-  <div className="discount-code-wrapper">
-    <div className="title-wrap">
-    <h4 className="font-semibold mb-[1rem] text-[1rem]">Use Coupon Code
-    </h4>
-    </div>
-    <div className="discount-code">
-    <p className="mb-[.8rem] text-[.9rem] text-gray-400">Enter your coupon code if you have one.
+        </div>
 
-</p>
-      <form>
-      <input type="text" className="w-[100%] border border-black py-[.3rem] px-3" placeholder="name"/>
-      <button className="cart-btn-2 mt-[1rem]" type="submit">Apply Coupon</button>
-      </form>
+        {/* Cart Total Section */}
+        <div className="xl:w-[35] 2xl:w-[28%]">
+          <div className="cart-tax border border-[#266bf9] shadow-sm bg-white px-[2rem] py-[2rem] text-black">
+            <h4 className="font-semibold text-[1rem]">Cart Total</h4>
+            <h5 className="flex py-[1rem] justify-between">
+              Total products <span className="font-semibold">$260.00</span>
+            </h5>
+            <div className="total-shipping">
+              <h5>Total shipping</h5>
+              <ul>
+                <li className="flex justify-between">
+                  <div>
+                    <input type="checkbox" /> Standard
+                  </div>
+                  <span className="text-gray-500">$20.00</span>
+                </li>
+                <li className="flex justify-between">
+                  <div>
+                    <input type="checkbox" /> Express
+                  </div>
+                  <span className="text-gray-500">$30.00</span>
+                </li>
+              </ul>
+                 {/* Coupon Section */}
+          <div className="cart-tax  bg-white pt-[1rem] text-black">
+            <div className="discount-code-wrapper">
+              <h4 className="font-semibold mb-[.5rem] text-[1rem]">Use Coupon Code</h4>
+      
+              <form className="flex mb-[.5rem] justify-between">
+  <input
+    type="text"
+    className="border w-[100%] border-[#266bf9] focus:outline-blue-800 py-[.5rem] px-3 h-[2.5rem]"  // Adjust the padding and height
+    placeholder="Coupon code"
+  />
+  <button
+    className="py-[.5rem] px-3 bg-[#266bf9] h-[2.5rem] ml-[.5rem] text-[white] hover:bg-[#266bf9] hover:text-white"
+    type="submit"
+  >
+    Apply
+  </button>
+</form>
+<div>
+<h5 className="flex pb-[1rem] justify-between">
+              Coupon <span className="font-semibold">0</span>
+            </h5>
+</div>
+            </div>
+          </div>
+            </div>
+            <h4 className="grand-total-title flex justify-between text-[1.1rem] pb-[2rem] font-semibold text-[#266bf9]">
+              Grand Total <span>$260.00</span>
+            </h4>
+            <a href="checkout.html" className="block text-center bg-[#266bf9] text-white py-2 text-[.9rem]">
+              Proceed to Checkout
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-<div className="cart-tax w-[30%]  border border-[#266bf9] shadow-sm h-[100%] bg-white px-[2rem] py-[2rem] text-black">
-  <div className="title-wrap">
-    <h4 className="font-semibold  text-[1rem]">Cart Total</h4>
-  </div>
-  <h5 className="flex py-[1rem] justify-between">Total products <span className="font-semibold">$260.00</span></h5>
-  <div className="total-shipping">
-    <h5>Total shipping</h5>
-    <ul>
-      <li className="flex justify-between"><div>
-      <input type="checkbox" /> Standard
-        </div> <span className="text-gray-500">$20.00</span></li>
-      <li className="flex justify-between">
-        <div>
-        <input type="checkbox" /> Express</div> <span className="text-gray-500">$30.00</span></li>
-    </ul>
-  </div>
-  <h4 className="grand-totall-title flex justify-between text-[1.1rem] py-[2rem] font-semibold text-[#266bf9]">Grand Total <span>$260.00</span></h4>
-  <a href="checkout.html" className="block text-center bg-[#266bf9] text-white py-2 text-[.9rem]">Proceed to Checkout</a>
-</div>
-</div>
-    </div>
-    ;
-  }
-  
+  );
+}
